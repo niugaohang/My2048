91c80639522e04712d3251ff36e82660d078e584
牛高航 <453460990@qq.com>
2016-09-05 14:11:54 +0800

    Initial Commit

---
+++ My2048/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  My2048
+//
+//  Created by 牛高航 on 16/9/5.
+//  Copyright © 2016年 牛高航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
